9325670ba8a7264bf9a2c1e7262c9154ceff9222
Yauheni Rabenki <Yauheni_Rabenki@epam.com>
2020-02-18 15:33:09 +0300

    FirstTest refactoring commit

--- FirstTest/PageObjects/YandexMainPage.js
+++ FirstTest/PageObjects/YandexMainPage.js
@@ -5,6 +5,10 @@ export class YandexMainPage{
     this.locationButton =  element(by.css("span.geolink__reg"));
     this.moreButton = element(by.css("a[class*='home-link home-link_blue_yes home-tabs__link home-tabs__more-switcher']"));
     }
+    async goToLocationPage(){
+        await isPresentWait(this.locationButton, 0.2, 3);
+        await this.locationButton.click();
+    }
     async expandMoreBar(){
         await isPresentWait(this.moreButton, 0.2, 3);
         await this.moreButton.click();
--- FirstTest/spec.js
+++ FirstTest/spec.js
@@ -11,26 +11,19 @@ describe('First test', function() {
     const moreBar = new MoreBar();
 
     const url = "https://yandex.by/";
-    let moreBarContentLondon;
-    let moreBarContentParis;
-
-    afterEach(function() {
-        console.log(moreBarContentLondon);
-        console.log(moreBarContentParis);
-    });
 
     it('First test', async function() {
         await browser.get(url);
         
-        await yandexMainPage.locationButton.click();
+        await yandexMainPage.goToLocationPage();
         await yandexLocationPage.setNewLocation("London");
-        await  yandexMainPage.expandMoreBar();
-        moreBarContentLondon =  await moreBar.moreBarElements.getText();
+        await yandexMainPage.expandMoreBar();
+        const moreBarContentLondon =  await moreBar.moreBarElements.getText();
         
-        await  yandexMainPage.locationButton.click();
+        await yandexMainPage.goToLocationPage();
         await  yandexLocationPage.setNewLocation("Paris");
         await  yandexMainPage.expandMoreBar();
-        moreBarContentParis =  await moreBar.moreBarElements.getText();
+        const moreBarContentParis =  await moreBar.moreBarElements.getText();
         
         expect(moreBarContentLondon).toEqual(moreBarContentParis);
     });
